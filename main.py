10f636377108d2a41f718dd2984384fafdeeed7a
Александр Рахмаев <rahmaevao@engee.cloud>
2023-07-21 21:55:25 +0300

    Исключаю устанволенные либы из дерева

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from src.deps import get_deps
 
-print(get_deps("/home/rahmaevao/Projects/konoha/administrator/main.py"))
+print(get_deps("/home/rahmaevao/Projects/konoha/administrator"))
--- src/deps.py
+++ src/deps.py
@@ -1,10 +1,12 @@
 import subprocess
 import json
+import tomli
 
 
-def get_deps(source_path: str) -> dict:
+def get_deps(srv_path: str) -> dict:
+    used_libs = _read_used_libraries(srv_path)
     result = subprocess.run(
-        f"poetry run pydeps {source_path} --max-bacon=10  --exclude fastapi pydantic matplotlib numpy sqlalchemy requests zmq markdown --show-deps --noshow --no-output",
+        f"poetry run pydeps {srv_path}/main.py --max-bacon=10  --exclude {' '.join(used_libs)} --show-deps --noshow --no-output",
         shell=True,
         capture_output=True,
         encoding="utf-8",
@@ -14,3 +16,9 @@ def get_deps(source_path: str) -> dict:
         raise RuntimeError(f"Pydeps failed {result.stderr}")
 
     return json.loads(result.stdout)
+
+
+def _read_used_libraries(srv_path: str) -> list[str]:
+    with open(f"{srv_path}/pyproject.toml", "rb") as f:
+        pyproject_toml = tomli.load(f)
+    return pyproject_toml["tool"]["poetry"]["dependencies"].keys()
