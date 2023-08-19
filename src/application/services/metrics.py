06e89e9315e35b86e7d0b2228082a682a8252446
Александр Рахмаев <rahmaevao@engee.cloud>
2023-08-19 11:32:14 +0300

    Рефакторинг метрик

--- src/application/services/metrics.py
+++ src/application/services/metrics.py
@@ -1,21 +1,13 @@
-"""
-Подробные метрики:
-- таблица метрик по компонентам
-
-Уточненные метрики:
-- проблемные модули (расстояние и дисперсия больше чем опр. число: зона бесполезности,
-зона боли)
-
-Короткие метрики:
-- среднее расстояние и дисперсия
-"""
-from functools import lru_cache
 from typing import NamedTuple
 import pandas as pd
 from src.application.project import PythonProject
 
 
 def compute_metrics(project: PythonProject) -> pd.DataFrame:
+    """Полные метрики по проекту
+
+    Возвращает датафрейм с метриками по компонентам.
+    """
     return pd.concat(
         [
             pd.DataFrame.from_dict(
@@ -38,6 +30,10 @@ class ShortMetrics(NamedTuple):
 
 
 def compute_short_metrics(project: PythonProject) -> ShortMetrics:
+    """Короткие метрики: среднее и дисперсия расстояние от главной прямой
+
+    Оба числа должны быть как можно ближе к нулю.
+    """
     return ShortMetrics(
         mean=compute_metrics(project)["D"].mean(),
         std=compute_metrics(project)["D"].std(),
