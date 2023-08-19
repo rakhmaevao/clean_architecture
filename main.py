5be89f11aee02b2e50b6b7cda25aef66ce06dfe7
Александр Рахмаев <rahmaevao@engee.cloud>
2023-08-19 11:38:33 +0300

    main файл дает мтерики в первую очередь

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from src.app import Application
+# from src.app import Application
 
-if __name__ == "__main__":
-    app = Application(micro_service_path="/home/rahmaevao/Projects/konoha/blocks")
-    app.run()
+# if __name__ == "__main__":
+#     app = Application(micro_service_path="/home/rahmaevao/Projects/konoha/blocks")
+#     app.run()
 
 
-# "/home/rahmaevao/Projects/konoha/blocks"
-# "/home/rahmaevao/Projects/konoha/administrator"
-# "/home/rahmaevao/Projects/konoha/filesystem"
+from pathlib import Path
+from src.application.services.metrics import compute_short_metrics
+from src.application.services.reader.reader import read_project
+
+
+projects = {
+    # "blocks": Path("/home/rahmaevao/Projects/konoha/blocks"),
+    # "administrator": Path("/home/rahmaevao/Projects/konoha/administrator"),
+    # "filesystem": Path("/home/rahmaevao/Projects/konoha/filesystem"),
+    "this": Path("/home/rahmaevao/Projects/clean_architecture"),
+}
+
+
+for project, path in projects.items():
+    metrics = compute_short_metrics(read_project(path))
+    print(f"{project}: {metrics}")
