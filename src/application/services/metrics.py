6ebfafdfc18bf3d526237e7c1a914323bce4a82b
Александр Рахмаев <rahmaevao@engee.cloud>
2023-08-19 11:30:06 +0300

    compute_short_metrics добавлен

--- src/application/services/metrics.py
+++ src/application/services/metrics.py
@@ -1,20 +1,18 @@
 """
-Короткие метрики:
-- среднее расстояние и дисперсия
+Подробные метрики:
+- таблица метрик по компонентам
 
 Уточненные метрики:
 - проблемные модули (расстояние и дисперсия больше чем опр. число: зона бесполезности,
 зона боли)
 
-Подробные метрики:
-- таблица метрик по компонентам
+Короткие метрики:
+- среднее расстояние и дисперсия
 """
+from functools import lru_cache
+from typing import NamedTuple
 import pandas as pd
 from src.application.project import PythonProject
-
-
-def compute_metrics(project: PythonProject):
-    pass
 
 
 def compute_metrics(project: PythonProject) -> pd.DataFrame:
@@ -31,4 +29,16 @@ def compute_metrics(project: PythonProject) -> pd.DataFrame:
             for module in project.modules.values()
         ],
         ignore_index=True,
+    )
+
+
+class ShortMetrics(NamedTuple):
+    mean: float
+    std: float
+
+
+def compute_short_metrics(project: PythonProject) -> ShortMetrics:
+    return ShortMetrics(
+        mean=compute_metrics(project)["D"].mean(),
+        std=compute_metrics(project)["D"].std(),
     )
--- tests/test_application/test_services/test_metrics.py
+++ tests/test_application/test_services/test_metrics.py
@@ -1,5 +1,9 @@
 import pandas as pd
-from src.application.services.metrics import compute_metrics
+from src.application.services.metrics import (
+    ShortMetrics,
+    compute_metrics,
+    compute_short_metrics,
+)
 from tests.test_application.const import MOCK_PROJECT
 from pandas.testing import assert_frame_equal
 
@@ -22,4 +26,10 @@ def test_compute_metrics():
                 "D": [1.0, 0.6666666666666667, 0.0, 0.5, 0.5, 0.5],
             }
         ),
+    )
+
+
+def test_compute_short_metrics():
+    assert compute_short_metrics(MOCK_PROJECT) == ShortMetrics(
+        mean=0.5277777777777778, std=0.3234650361779919
     )
