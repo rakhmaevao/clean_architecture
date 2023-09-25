e57b0a2250392a24ee1ac04ee17836fec5e8ff6e
Александр Рахмаев <rahmaevao@engee.cloud>
2023-09-25 20:38:01 +0300

    Форматирвоание

--- src/application/services/reader/inspector.py
+++ src/application/services/reader/inspector.py
@@ -21,7 +21,6 @@ class ClassSearchingResult:
     using_modules_paths: set[Path]
 
 
-
 def get_all_classes(project_path: Path) -> list[ClassSearchingResult]:
     classes: dict[ClassName, ClassSearchingResult] = dict()
     origin_sys_path = copy.deepcopy(sys.path)
